eb00e4d9fd3593523fce7a72aaa3871283f2f922
Clay <clay.stup@gmail.com>
2020-08-04 18:41:40 -0700

    test fodlers

---
+++ web/pages/garage/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+import {
+  AuthTokens,
+  useAuth,
+  useAuthFunctions,
+  getServerSideAuth
+} from '../../auth'
+
+
+async function openGarage(token) {
+  const result = await fetch('/api/garage/garagedoor', { headers: new Headers({ 'authorization': token})})
+  const data = await result.json()
+  console.log(data)
+}
+
+
+const Home = (props: { initialAuth: AuthTokens }) => {
+  const auth = useAuth(props.initialAuth)
+  const { login, logout } = useAuthFunctions()
+  return (
+    <React.Fragment>
+      {auth ? (
+        <div>
+          <style jsx>{`
+          button {
+            margin: 40px;
+          } 
+          `}</style>
+          <div>
+            <button type="button" onClick={() => logout()}>
+              sign out
+            </button>
+          </div>
+          <div>
+            FROM GARAGE FOLDER
+          </div>
+          <div>
+            <button className="button" type="button" onClick={async () => await openGarage(auth.accessToken)}>Open Dublin Garage Door</button>
+          </div>
+        </div>
+      ) : (
+        <React.Fragment>
+          <button type="button" onClick={() => login()}>
+            sign in
+          </button>
+        </React.Fragment>
+      )
+      }
+    </React.Fragment>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  initialAuth: AuthTokens
+}> = async (context) => {
+  const initialAuth = getServerSideAuth(context.req)
+  return { props: {initialAuth}}
+}
+export default Home
